1bd9ea087d6cad46749bb7b1c85055cdfb2d10b0
PauloAcca <paulo.matias.accardo@gmail.com>
2026-07-24 23:09:15 -0300

    feat: install and configure @nestjs/config to enable global environment variable support

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
+import { ConfigModule } from '@nestjs/config';
 import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     ScheduleModule.forRoot(),
     ProductsModule,
   ],
